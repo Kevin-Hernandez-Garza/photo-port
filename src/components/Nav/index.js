10fdac2e49aeb59e433dc4bad0eb49d804936ad1
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2021-12-16 15:42:39 -0600

    implement hooks

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,31 +1,53 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
 
 // nav component
-function Nav() {
-    const categories = [
-        {
-            name: 'commercial',
-            description: 
-                'Photos of grocery stores, food trucks, and other commercial projects',
-        },
-        { name: 'portraits', description: 'Portraits of people in my life'},
-        { name: 'food', description: 'Delicious delicacies'},
-        {
-            name: 'landscape', 
-            description: 'Fields, farmhouses, waterfalls, and the beauty of nature',
-        },
-    ];
+function Nav(props) {
+    const {
+        categories = [],
+        setCurrentCategory,
+        currentCategory
+    } = props;
 
-    function categorySelected(name) {
-        console.log(`${name} clicked`)
-    }
+    // useEffect hook
+    useEffect(() => {
+        document.title = capitalizeFirstLetter(currentCategory.name);
+    }, [currentCategory]);
+
+    // // initializing the category state
+    // const [currentCategory, setCurrentCategory] = useState(categories[0]);
+    // // useState hook
+    // const [categories] = useState([
+    //     {
+    //         name: 'commercial',
+    //         description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    //     },
+    //     {
+    //         name: 'portraits',
+    //         description: 'Portraits of people in my life'
+    //     },
+    //     {
+    //         name: 'food',
+    //         description: 'Delicious delicacies'
+    //     },
+    //     {
+    //         name: 'landscape',
+    //         description: 'Fields, farmhouses, waterfalls, and the beauty of nature'
+    //     },
+    // ]);
+
+    // function categorySelected(name) {
+    //     console.log(`${name} clicked`)
+    // }
 
     return (
-        <header>
+        <header className='flex-row px-1'>
             <h2>
                 <a data-testid="link" href="/">
-                    <span role="img" aria-label="camera">📸</span>Oh Snap!
+                    <span role="img" aria-label="camera">
+                    📸</span>
+                    Oh Snap!
                 </a>
             </h2>
 
@@ -36,13 +58,16 @@ function Nav() {
                             About Me
                         </a>
                     </li>
-                    <li>
+                    <li className='mx-2'>
                        <span>Contact</span> 
                     </li>
                     {categories.map((category) => (
-                        <li className='mx-1' key={category.name}>
-                            <span onClick={() => categorySelected(category.name)} >
-                                {category.name}
+                        // short-circuit expression
+                        <li className={`mx-1 ${
+                            currentCategory.name === category.name && 'navActive'}`
+                        } key={category.name}>
+                            <span onClick={() => { setCurrentCategory(category)}} >
+                                {capitalizeFirstLetter(category.name)}
                             </span>
                         </li>
                     ))}
