e3a91d14bbc20d1c90e0c3634b7c5b88daafed42
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2021-12-16 17:41:51 -0600

    more tests

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,21 +1,15 @@
 import React from 'react';
+import PhotoList from '../PhotoList';
 import { capitalizeFirstLetter } from '../../utils/helpers';
-import photo from '../../assets/small/commercial/0.jpg';
 
-function Gallery(props) {
-    const currentCategory = {
-        name: 'commercial',
-        description: 'Photos of grocery stores, food trucks, and other commercial projects',
-    };
+function Gallery({ currentCategory }) {
+    const { name, description } = currentCategory;
 
     return (
         <section>
-            <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
-            <p>{currentCategory.name}</p>
-
-            <div className='flex-row'>
-                <img src={photo} alt='commercial example' className='img-thumbnail mx-1' />
-            </div>
+            <h1 data-testid='h1tag'>{capitalizeFirstLetter(name)}</h1>
+            <p>{description}</p>
+            <PhotoList category={currentCategory.name} />
         </section>
     );
 }
--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -3,18 +3,33 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
 
+// props being declared 
+const categories = [
+    {name: 'portraits', description: 'Portraits of people in my life'}
+]
+
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn(); 
+
 afterEach(cleanup);
 
 // nav component
 describe('Nav component', () => {
     // baseline test
     it('renders', () => {
-        render(<Nav />);
+        render(<Nav 
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+        />);
     });
 
     // snapshot test
     it('matches snapshot', () => {
-        const { asFragment } = render(<Nav />);
+        const { asFragment } = render(<Nav 
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}/>);
         // assert value comparison
         expect(asFragment()).toMatchSnapshot();
     });
@@ -25,7 +40,10 @@ describe('Nav component', () => {
 describe('emoji is visible', () => {
     it('inserts emoji into the h2', () => {
         // Arrange
-        const { getByLabelText } = render(<Nav />);
+        const { getByLabelText } = render(<Nav 
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}/>);
         // Assert
         expect(getByLabelText('camera')).toHaveTextContent('📸')
     });
@@ -36,7 +54,10 @@ describe('links are visible', () => {
     // "it" single test case
     it('inserts text into the links', () => {
         // arrange queries
-        const { getByTestId } = render(<Nav />);
+        const { getByTestId } = render(<Nav 
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}/>);
         // assert
         expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
         expect(getByTestId('about')).toHaveTextContent('About Me');
