d87a251d185bb29a3f12e462844d5da7eb8157a3
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2021-12-16 18:00:23 -0600

    contact form

---
+++ src/components/Contact/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function ContactForm() {
+    // JSX
+    return (
+        <section>
+            <h1>Contact me</h1>
+            <form id='contact-form'>
+                {/* name input */}
+                <div>
+                    <label htmlFor='name'>Name:</label>
+                    <input type='text' name='name' /> 
+                </div>
+                {/* email input */}
+                <div>
+                    <label htmlFor='email'>Email address:</label>
+                    <input type='email' name='email' /> 
+                </div>
+                {/* message text area */}
+                <div>
+                    <label htmlFor='message'>Message:</label>
+                    <textarea name='message' rows='5' />
+                </div>
+                
+                <button type='submit'>Submit</button>
+            </form>
+        </section>
+    );
+}
+
+export default ContactForm;
